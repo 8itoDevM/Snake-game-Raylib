d663c19d49c06ea293a6f4887cd09b783a3369f2
8ito <gustavomarcolino862@gmail.com>
2025-06-19 21:47:34 -0300

    Implement basic food rendering and memory management

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include "raylib.h"
 
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
+int cellSize = 30;
+int cellCount = 25;
+
+class Food{
+public:
+    Vector2 pos = {5, 6};
+    Texture2D texture;
+
+    Food(){
+        Image image = LoadImage("Graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+    }
+
+    ~Food(){
+        UnloadTexture(texture);
+    }
+    
+    void Draw(){
+        DrawTexture(texture, pos.x * cellSize, pos.y * cellSize, WHITE);
+    }
+};
+
 int main(){
-    InitWindow(800, 600, "My First game");
+    InitWindow(cellSize*cellCount, cellSize*cellCount, "Snake");
     SetTargetFPS(60);
+
+    Food food = Food();
 
     // GameLoop
     while(WindowShouldClose() == false){
@@ -12,6 +40,11 @@ int main(){
 
         // 3. Drawing
         BeginDrawing();
+
+        ClearBackground(green);
+
+        food.Draw();
+
         EndDrawing();
     }
 
