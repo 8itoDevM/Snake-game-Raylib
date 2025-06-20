1afc0087381aec52134dfe3a8bb1061e44197083
8ito <gustavomarcolino862@gmail.com>
2025-06-19 23:51:56 -0300

    Add Snake class and implement drawing functionality

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include "raylib.h"
+#include <deque>
+#include <iostream>
+
+using namespace std;
 
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
@@ -6,23 +10,53 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30;
 int cellCount = 25;
 
+class Snake{
+public:
+    deque<Vector2> body = {Vector2{6, 9}, Vector2{7,9}, Vector2{8, 9}};   
+
+    void Draw(){
+        for(unsigned int i = 0; i < body.size(); i++){
+            float x = body[i].x;
+            float y = body[i].y; 
+
+            Rectangle segment = Rectangle{
+                (x * cellSize),
+                (y * cellSize),
+                static_cast<float>(cellSize),
+                static_cast<float>(cellSize)
+            };
+
+            DrawRectangleRounded(segment, 0.5f, 6, darkGreen);
+        }
+    }
+};
+
 class Food{
 public:
-    Vector2 pos = {5, 6};
+    Vector2 pos;
     Texture2D texture;
 
     Food(){
         Image image = LoadImage("Graphics/food.png");
         texture = LoadTextureFromImage(image);
         UnloadImage(image);
+
+        pos = GenerateRandomPos();
     }
 
     ~Food(){
         UnloadTexture(texture);
     }
-    
+
     void Draw(){
         DrawTexture(texture, pos.x * cellSize, pos.y * cellSize, WHITE);
+    }
+
+    Vector2 GenerateRandomPos(){
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+
+        return Vector2{x, y};
     }
 };
 
@@ -31,6 +65,7 @@ int main(){
     SetTargetFPS(60);
 
     Food food = Food();
+    Snake snake = Snake();
 
     // GameLoop
     while(WindowShouldClose() == false){
@@ -44,6 +79,7 @@ int main(){
         ClearBackground(green);
 
         food.Draw();
+        snake.Draw();
 
         EndDrawing();
     }
